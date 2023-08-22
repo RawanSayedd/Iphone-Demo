a2fccb6e5bc956b7f6e421d8fea07ceb6d30d339
RawanSayedd <rawansayed143@gmail.com>
2023-08-22 22:45:48 +0300

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+let phone = document.querySelector(".img");
+function exchangePhone(image) {
+  phone.src = image;
+}
+let container = document.querySelector(".main-container");
+function backgroundColors(color) {
+  container.style.background = color;
+}
